7496f33d4ee1ad3aa4c515c49eb51ea1a2558c37
Gustavo <gustavoquintero2000@outlook.com>
2021-05-31 13:04:09 -0500

    Initial commit

---
+++ vistas/contenidos/es/web-index.php
@@ -0,0 +1,138 @@
+<div class="banner">
+    <div class="banner-body">
+        <h3 class="text-uppercase">Bienvenido a <?php echo COMPANY; ?></h3>
+        <p>Los mejores productos y la mejor calidad los encuentras en <?php echo COMPANY; ?></p>
+    </div>
+</div>
+
+<div class="container-fluid container-web-page border-bottom bg-white">
+    <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Nuestros servicios</h3>
+    <br>
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-shipping-fast fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Envíos a domicilio</h5>
+            <p class="text-center">Contamos con envios a todo mexico con entregas en menos de 24 horas hasta la puerta de su domicilio</p>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-boxes fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Ventas al por mayor</h5>
+            <p class="text-center">somos mayoristas, si usted es una pequeña farmacia contactenos y nosotros le ofreceremos los mejores precios de mayoristas</p>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-store-alt fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Retiro en tienda</h5>
+            <p class="text-center">contamos con el servicio de entrega en surcusal si es que no quiere esperar o simplemente por ahorrarse el envio</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="container-fluid container-web-page border-bottom bg-white">
+    <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Nuestros medicamentos más consumidos</h3>
+    <div class="container-cards full-box mb-4">
+
+        <div class="card-product div-bordered bg-white shadow-2">
+            <figure class="card-product-img">
+                <img class="img-fluid" src="<?php echo SERVERURL; ?>vistas/assets/product/product.jpg" alt="nombre_producto">
+            </figure>
+            <div class="card-product-body">
+                <div class="card-product-content scroll">
+                    <h5 class="text-center fw-bolder">Paracetamol</h5>
+                    <p class="card-product-price text-center fw-bolder">$150.00 MXN</p>
+                    <span class="full-box text-center text-muted" style="display: block;">En stock</span>
+                </div>
+                <div class="text-center card-product-options" style="padding: 10px 0;">
+                    <button type="button" class="btn btn-link btn-sm btn-rounded text-success" ><i class="fas fa-shopping-bag fa-fw"></i> &nbsp; Agregar</button>
+                    &nbsp; &nbsp;
+                    <a href="<?php echo SERVERURL; ?>details/" class="btn btn-link btn-sm btn-rounded" ><i class="fas fa-box-open fa-fw"></i> &nbsp; Detalles</a>
+                    &nbsp; &nbsp;
+                    <button type="button" class="btn btn-link btn-sm btn-rounded" ><i class="fas fa-heart fa-fw"></i></button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-product div-bordered bg-white shadow-2">
+            <figure class="card-product-img">
+                <img class="img-fluid" src="<?php echo SERVERURL; ?>vistas/assets/img/img_not_found.jpg" alt="nombre_producto">
+            </figure>
+            <div class="card-product-body">
+                <div class="card-product-content scroll">
+                    <h5 class="text-center fw-bolder">Aspirina</h5>
+                    <p class="card-product-price text-center fw-bolder">$60.00 MXN</p>
+                    <span class="full-box text-center text-muted" style="display: block;">En stock</span>
+                </div>
+                <div class="text-center card-product-options" style="padding: 10px 0;">
+                    <button type="button" class="btn btn-link btn-sm btn-rounded text-success" ><i class="fas fa-shopping-bag fa-fw"></i> &nbsp; Agregar</button>
+                    &nbsp; &nbsp;
+                    <a href="<?php echo SERVERURL; ?>details/" class="btn btn-link btn-sm btn-rounded" ><i class="fas fa-box-open fa-fw"></i> &nbsp; Detalles</a>
+                    &nbsp; &nbsp;
+                    <button type="button" class="btn btn-link btn-sm btn-rounded text-muted" ><i class="fas fa-heart fa-fw"></i></button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-product div-bordered bg-white shadow-2">
+            <figure class="card-product-img">
+                <img class="img-fluid" src="<?php echo SERVERURL; ?>vistas/assets/product/product.jpg" alt="nombre_producto">
+            </figure>
+            <div class="card-product-body">
+                <div class="card-product-content scroll">
+                    <h5 class="text-center fw-bolder">Clonazepam</h5>
+                    <p class="card-product-price text-center fw-bolder">$780.00 MXN</p>
+                    <span class="full-box text-center text-muted" style="display: block;">En stock</span>
+                </div>
+                <div class="text-center card-product-options" style="padding: 10px 0;">
+                    <button type="button" class="btn btn-link btn-sm btn-rounded text-success" ><i class="fas fa-shopping-bag fa-fw"></i> &nbsp; Agregar</button>
+                    &nbsp; &nbsp;
+                    <a href="<?php echo SERVERURL; ?>details/" class="btn btn-link btn-sm btn-rounded" ><i class="fas fa-box-open fa-fw"></i> &nbsp; Detalles</a>
+                    &nbsp; &nbsp;
+                    <button type="button" class="btn btn-link btn-sm btn-rounded" ><i class="fas fa-heart fa-fw"></i></button>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-product div-bordered bg-white shadow-2">
+            <figure class="card-product-img">
+                <img class="img-fluid" src="<?php echo SERVERURL; ?>vistas/assets/img/img_not_found.jpg" alt="nombre_producto">
+            </figure>
+            <div class="card-product-body">
+                <div class="card-product-content scroll">
+                    <h5 class="text-center fw-bolder">Omeoprazol</h5>
+                    <p class="card-product-price text-center fw-bolder">$30.00 MXN</p>
+                    <span class="full-box text-center text-muted" style="display: block;">En stock</span>
+                </div>
+                <div class="text-center card-product-options" style="padding: 10px 0;">
+                    <button type="button" class="btn btn-link btn-sm btn-rounded text-success" ><i class="fas fa-shopping-bag fa-fw"></i> &nbsp; Agregar</button>
+                    &nbsp; &nbsp;
+                    <a href="<?php echo SERVERURL; ?>details/" class="btn btn-link btn-sm btn-rounded" ><i class="fas fa-box-open fa-fw"></i> &nbsp; Detalles</a>
+                    &nbsp; &nbsp;
+                    <button type="button" class="btn btn-link btn-sm btn-rounded text-muted" ><i class="fas fa-heart fa-fw"></i></button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <p class="text-center"><a href="<?php echo SERVERURL; ?>product/all/" class="btn btn-primary"><i class="fas fa-box"></i> &nbsp; Ver productos</a></p>
+</div>
+
+
+<div class="container container-web-page">
+    <div class="row justify-content-md-center">
+        <div class="col-12 col-md-6">
+            <figure class="full-box">
+                <img src="<?php echo SERVERURL; ?>vistas/assets/img/registration.png" alt="registration_<?php echo COMPANY; ?>" class="img-fluid">
+            </figure>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-12 col-md-6">
+            <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Crea tu cuenta</h3>
+            <p class="text-justify">
+                Crea tu cuenta para poder realizar pedidos de productos hasta la puesta de tu casa, es muy fácil y rápido.
+            </p>
+            <p class="text-center">
+                <a href="<?php echo SERVERURL; ?>registration/" class="btn btn-primary" >Crear cuenta</a>
+            </p>
+        </div>
+    </div>
+</div>
